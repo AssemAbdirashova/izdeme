9daf7451fa10b54ce1c091a602593f782c8a2a50
AssemAbdirashova <47333371+AssemAbdirashova@users.noreply.github.com>
2020-10-28 10:40:54 +0300

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from rest_framework_jwt.views import obtain_jwt_token
 
-from api.views import StatisticsListAPIView, ServiceListAPIView, ServiceDetailAPIView, StatisticsDetailAPIView, open_csv
+from api.views import StatisticsListAPIView, ServiceListAPIView, ServiceDetailAPIView, StatisticsDetailAPIView, open_csv, CourseListAPIView, CourseDetailAPIView
 
 urlpatterns = [
      path('login/', obtain_jwt_token),
